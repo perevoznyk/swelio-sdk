81645ab6cd74c2017aaa2b3f89d872894c566a1a
Serhiy Perevoznyk <s.perevoznyk@gmail.com>
2020-08-06 17:07:51 +0200

    Added container class

---
+++ Swelio.Engine/Swelio.Engine/DocumentContainer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swelio.Engine
+{
+    public class DocumentContainer
+    {
+        private IntPtr ctx;
+
+        public DocumentContainer() : base()
+        {
+
+        }
+
+        ~DocumentContainer()
+        {
+            Close();
+        }
+
+        public bool AddFile(string fileName)
+        {
+            return false;
+        }
+
+        public bool Save(string fileName, int readerNumber)
+        {
+            return false;
+        }
+
+        public void Close()
+        {
+
+        }
+    }
+}
