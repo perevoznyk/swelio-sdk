7414a50bedd61c9b4a4209d6e9467fd244135519
Serhiy Perevoznyk <s.perevoznyk@gmail.com>
2021-12-06 14:05:31 +0100

    1.18.15

---
+++ C++/include/Network.h
@@ -0,0 +1,26 @@
+//===============================================================================
+//= Copyright (c) Serhiy Perevoznyk.  All rights reserved.
+//= THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+//= OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+//= LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+//= FITNESS FOR A PARTICULAR PURPOSE.
+//===============================================================================
+
+#pragma once
+
+#include <windows.h>
+
+#if !defined( _NETWORK_H_ )
+#define _NETWORK_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	BOOL WINAPI ServerAccessible(LPWSTR name);
+#ifdef __cplusplus
+}
+#endif
+
+#endif     // __NETWORK_H__
+
+
